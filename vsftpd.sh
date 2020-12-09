b95084a3bbdaf886f88858cbe49b0622aba207fa
Dan Vogel <dan.vogel@gmail.com>
2020-12-08 20:40:36 -0500

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -16,6 +16,7 @@ sudo chown -R apache:apache /var/www/html
 #
 # edit vsftpd.conf to enable passive ftp
 #
+sudo cp /etc/vsftpd/vsftp.conf /etc/vsftpd/vsftpd.conf.bak
 echo "### enable ftp for wordpress updates ###" | sudo tee -a /etc/vsftpd.vsftpd.conf
 echo "pasv_enable=YES" | sudo tee -a /etc/vsftpd/vsftpd.conf
 echo "pasv_min_port=64000" | sudo tee -a /etc/vsftpd/vsftpd.conf
