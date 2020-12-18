1c543f38f66b353d47a44c83fdbe92fdba260ecc
Cloud User <centos@www.datos.asia>
2020-12-18 17:46:06 +0000

    added sasl_passwd section

--- aws_ses.sh
+++ aws_ses.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 region="ap-northeast-1"
 smtp_host="email-smtp.${region}.amazonaws.com"
+#
 echo "Setting postfix to use $smtp_host"
 #
 sudo postconf -e "relayhost = [${smtp_host}]:587" \
@@ -10,3 +11,17 @@ sudo postconf -e "relayhost = [${smtp_host}]:587" \
 "smtp_use_tls = yes" \
 "smtp_tls_security_level = encrypt" \
 "smtp_tls_note_starttls_offer = yes"
+#
+# FYI only
+# This section was not coded since the entry did not exist in my lightsail master.cf
+# In a text editor, open the file /etc/postfix/master.cf.
+# Search for the following entry:
+#
+#   -o smtp_fallback_relay=
+#
+#  If you find this entry, comment it out by placing a # (hash) character 
+#  at the beginning of the line. Save and close the file. 
+#
+read -p 'SMTPUSERNAME ? ' SMTPUSERNAME
+read -p 'SMTPPASSWORD ? ' SMTPPASSWORD
+echo "[${smtp_host}]:587 ${SMTPUSERNAME}:${SMTPPASSWORD}"|sudo tee /etc/postfix/sasl_passwd
