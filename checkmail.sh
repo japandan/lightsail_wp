85fddb3a399e950284ace98b62d1b61572b1f9c4
Dan Vogel <dan.vogel@gmail.com>
2021-10-19 12:14:09 -0400

    Create checkmail.sh

---
+++ checkmail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+systemctl status nginx
+systemctl status mariadb
+systemctl status postfix
+systemctl status iredapd
+systemctl status dovecot
+systemctl status clamav
+systemctl status slapd
