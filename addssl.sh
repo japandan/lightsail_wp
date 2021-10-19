56c8064263c7cd2c7a1a598ff36fa718af2ea8c4
Dan Vogel <dan.vogel@gmail.com>
2021-10-19 11:36:14 -0400

    Update addssl.sh

--- addssl.sh
+++ addssl.sh
@@ -2,28 +2,22 @@
 #
 # this will install certbot and create ssl certificate for the new site
 yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
-sudo yum install -y certbot python2-certbot-apache bind-utils
-## make sure that you have virtual host config in apache for example /etc/httpd/conf.d/vhosts.conf
-sudo cat >/etc/httpd/conf.d/vhosts.conf <<'EOF'
-<VirtualHost *:80>
- DocumentRoot "/var/www/html"
- ServerName mail.datostech.com
- ServerAlias datos.asia
- ServerAlias www.datos.asia
- ServerAlias datostech.com
- ServerAlias www.datostech.com
-</VirtualHost>
-EOF
-sudo systemctl restart httpd
-certbot --apache
-#Congratulations! You have successfully enabled https://aws.datos.asia and https://vhost1.datos.asia
-#You should test your configuration at:
-#https://www.ssllabs.com/ssltest/analyze.html?d=aws.datos.asia
-#https://www.ssllabs.com/ssltest/analyze.html?d=vhost1.datos.asia
+sudo yum install -y certbot bind-utils
+## Add the server names for the ssl
+sed -i 's/server_name _;/server_name datos.asia www.datos.asia mail.datostech.com www.datostech.com;/' /etc/nginx/sites-enabled/00-default.conf 
+sed -i 's/server_name _;/server_name datos.asia www.datos.asia mail.datostech.com www.datostech.com;/' /etc/nginx/sites-enabled/00-default-ssl.conf
+systemctl restart nginx
 #
-#Congratulations! Your certificate and chain have been saved at:   
-#/etc/letsencrypt/live/aws.datos.asia/fullchain.pem   
-#Your key file has been saved at:   /etc/letsencrypt/live/aws.datos.asia/privkey.pem
-#Your cert will expire on 2020-03-08. To obtain a new or tweaked   version of this certificate in the future, 
-#simply run certbot again   with the "certonly" option. 
-#To non-interactively renew *all* of   your certificates, run "certbot renew" 
+certbot --nginx -d datos.asia -d www.datos.asia -d mail.datostech.com -d www.datostech.com 
+#Congratulations! You have successfully enabled https://aws.datos.asia and https://vhost1.datos.asia
+#
+echo You should test your configuration at:
+echo https://www.ssllabs.com/ssltest/analyze.html?d=datos.asia
+echo https://www.ssllabs.com/ssltest/analyze.html?d=mail.datostech.com
+echo https://www.ssllabs.com/ssltest/analyze.html?d=www.datostech.com
+#
+#   
+echo To obtain a new or tweaked   version of this certificate in the future, 
+echo simply run certbot again   with the "certonly" option. 
+echo To non-interactively renew *all* of   your certificates, run "certbot renew" 
+echo done.
