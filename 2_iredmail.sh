06324d10c8a2be087baa9186faf21ba1dbad54c8
Dan Vogel <dan.vogel@gmail.com>
2021-10-21 13:46:15 -0400

    Update 2_iredmail.sh

--- 2_iredmail.sh
+++ 2_iredmail.sh
@@ -41,9 +41,9 @@ wget https://github.com/iredmail/iRedMail/archive/1.4.2.tar.gz
 tar -xzvf 1.4.2.tar.gz
 echo "cd into directory and type #bash iRedMail.sh"
 #
-cp -f lightsail_wp/config iRedMail*/
 MySQLPassword=$(cat /root/MySQLPassword )
-sed -i "s/ChangeM3/$MySQLPassword/" iRedMail*/config
+sed -i "s/ChangeM3/$MySQLPassword/" lightsail_wp/config
+cp -f lightsail_wp/config iRedMail*/
 cd iRedMail*
 #
 # Run this unattended
