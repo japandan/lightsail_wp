86fa3ff61de584e5c5c8dad87d026ba2882b693f
Dan Vogel <dan.vogel@gmail.com>
2021-10-21 12:17:10 -0400

    Update 1_LAMPWP.sh

--- 1_LAMPWP.sh
+++ 1_LAMPWP.sh
@@ -6,11 +6,18 @@
 #
 echo "Begin LAMPWP setup script for MariaDB,PHP 7.3 and Wordpress">>/root/INSTALL.LOG
 echo "Installing mariadb(mysql)" >>/root/INSTALL.LOG
+echo "MySQL root password is $1"
 # This is mysql
 yum install -y epel-release mariadb-server
 systemctl start mariadb
 systemctl enable mariadb
 echo "there is no root password on mysql yet" >>/root/INSTALL.LOG
+mysql -e "UPDATE mysql.user SET Password=PASSWORD($1) WHERE User=’root’;"
+mysql -e "DELETE FROM mysql.user WHERE User=’root’ AND Host NOT IN (‘localhost’, ‘127.0.0.1’, ‘::1’);"
+mysql -e "DELETE FROM mysql.user WHERE User=”;"
+mysql -e "DROP DATABASE test;"
+mysql -e "FLUSH PRIVILEGES;"
+#
 echo "mysql_secure_installation should be run manually or automated.">>/root/INSTALL.LOG
 #
 # TMUX is a handy program to run in case you have scripts to run and need to disconnect
