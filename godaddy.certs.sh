a38295e9ac9a3a5ee80b12be69a674b409b3dcd2
Dan Vogel <dan.vogel@gmail.com>
2021-10-22 10:46:13 -0400

    Create godaddy.certs.sh

---
+++ godaddy.certs.sh
@@ -0,0 +1,14 @@
+/etc/pki/tls/certs
+-rw-r--r-- 1 root root   2220 Jan 26  2021 7f307e6692e4f1c7.crt
+-rw-r--r-- 1 root root 761469 Oct  3  2019 ca-bundle.crt
+-rw-r--r-- 1 root root 442272 Oct  3  2019 ca-bundle.trust.crt
+lrwxrwxrwx 1 root root     20 Jan 26  2021 cert.pem -> 7f307e6692e4f1c7.crt
+lrwxrwxrwx 1 root root     19 Jan 26  2019 fullchain.pem -> gd_bundle-g2-g1.crt
+-rw-r--r-- 1 root root   4795 Jan 26  2021 gd_bundle-g2-g1.crt
+lrwxrwxrwx 1 root root     13 Feb 18  2021 iRedMail.crt -> fullchain.pem
+-rw------- 1 root root   1452 Dec  4  2017 localhost.crt
+https://github.com/japandan
+/etc/pki/tls/private
+lrwxrwxrwx 1 root root   11 Feb 18  2021 iRedMail.key -> privkey.pem
+-rw------- 1 root root 1675 Dec  4  2017 localhost.key
+-rw-r--r-- 1 root root 1704 Jan 25  2018 privkey.pem
